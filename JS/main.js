6f0179509cc652b71750d7c255e954655f7486f4
Scott Schumacker <scottschu97@gmail.com>
2024-02-08 12:31:35 -0500

    Updated API

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,9 @@
 import NotesAPI from "./NotesAPI.js"
+
+NotesAPI.saveNote({
+    id: 514919,
+    title: "The title has changed",
+    body: "This is a second new note."
+});
+
+console.log(NotesAPI.getAllNotes());
