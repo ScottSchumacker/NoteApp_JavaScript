a6bb08383cc9525834d808561d295732d6c47ad0
Scott Schumacker <scottschu97@gmail.com>
2024-02-08 13:08:58 -0500

    Updated JS View with event listeners

--- JS/main.js
+++ JS/main.js
@@ -4,5 +4,10 @@ const app = document.getElementById("app");
 const view = new NotesView(app, {
     onNoteAdd() {
         console.log("Lets add a new note!");
+    },
+    onNoteEdit(newTitle, newBody) {
+        console.log(newTitle);
+        console.log(newBody);
     }
+
 });
