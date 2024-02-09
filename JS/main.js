0f59630a58ea8a05e5d5a7d6c6b5bf80747b3296
Scott Schumacker <scottschu97@gmail.com>
2024-02-09 11:35:22 -0500

    Updated app JS file

--- JS/main.js
+++ JS/main.js
@@ -1,25 +1,4 @@
-import NotesView from "./NotesView.js"
-import NotesAPI from "./NotesAPI.js"
+import App from "./app.js";
 
-const app = document.getElementById("app");
-const view = new NotesView(app, {
-    onNoteAdd() {
-        console.log("Lets add a new note!");
-    },
-    onNoteSelect() {
-        console.log("Note Select: " + id);
-    },
-    onNoteDelete() {
-        console.log("Note Deleted: " + id);
-    },
-    onNoteEdit(newTitle, newBody) {
-        console.log(newTitle);
-        console.log(newBody);
-    }
-
-});
-
-const notes = NotesAPI.getAllNotes();
-
-view.updateNotesList(notes);
-view.updateActiveNote(notes[0]);
+const root = document.getElementById("app");
+const app = new App(root);
